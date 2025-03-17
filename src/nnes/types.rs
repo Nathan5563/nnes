29266ddbe139adfa3b665ed17662dce235fc0885
Nathan5563 <ndawit611@gmail.com>
2025-03-17 13:00:15 -0400

    Added macros to refer to bits

--- src/nnes/types.rs
+++ src/nnes/types.rs
@@ -1,2 +1,16 @@
 pub static BYTE: u8 = 0x8;
 pub static WORD: u16 = 0x10;
+
+pub static BIT_0: u8 = 0b00000001;
+pub static BIT_1: u8 = 0b00000010;
+pub static BIT_2: u8 = 0b00000100;
+pub static BIT_3: u8 = 0b00001000;
+pub static BIT_4: u8 = 0b00010000;
+pub static BIT_5: u8 = 0b00100000;
+pub static BIT_6: u8 = 0b01000000;
+pub static BIT_7: u8 = 0b10000000;
+
+pub static LOWER_BYTE: u8 = 0b00001111;
+pub static UPPER_BYTE: u8 = 0b11110000;
+pub static LOWER_NIBBLE: u8 = 0b0011;
+pub static UPPER_NIBBLE: u8 = 0b1100;
