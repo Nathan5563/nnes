4f11473d164d312ef39d39cc756ce846fbacfa01
Nathan5563 <ndawit611@gmail.com>
2025-03-18 12:21:43 -0400

    Corrected byte/nibble definitions

--- src/nnes/types.rs
+++ src/nnes/types.rs
@@ -10,7 +10,7 @@ pub static BIT_5: u8 = 0b00100000;
 pub static BIT_6: u8 = 0b01000000;
 pub static BIT_7: u8 = 0b10000000;
 
-pub static LOWER_BYTE: u8 = 0b00001111;
-pub static UPPER_BYTE: u8 = 0b11110000;
-pub static LOWER_NIBBLE: u8 = 0b0011;
-pub static UPPER_NIBBLE: u8 = 0b1100;
+pub static LOWER_NIBBLE: u8 = 0b00001111;
+pub static UPPER_NIBBLE: u8 = 0b11110000;
+pub static LOWER_BYTE: u16 = 0b0000000011111111;
+pub static UPPER_BYTE: u16 = 0b1111111100000000;
