30111c03f8d3ad045d8956d6aa93ce77f20bdae8
Nathan5563 <ndawit611@gmail.com>
2025-03-23 13:56:32 -0400

    Added lower/upper word types

--- src/types.rs
+++ src/types.rs
@@ -10,7 +10,9 @@ pub static BIT_5: u8 = 0b00100000;
 pub static BIT_6: u8 = 0b01000000;
 pub static BIT_7: u8 = 0b10000000;
 
-pub static LOWER_NIBBLE: u8 = 0b00001111;
-pub static UPPER_NIBBLE: u8 = 0b11110000;
-pub static LOWER_BYTE: u16 = 0b0000000011111111;
-pub static UPPER_BYTE: u16 = 0b1111111100000000;
+pub static LOWER_NIBBLE: u8 = 0x0f;
+pub static UPPER_NIBBLE: u8 = 0xf0;
+pub static LOWER_BYTE: u16 = 0x00ff;
+pub static UPPER_BYTE: u16 = 0xff00;
+pub static LOWER_WORD: u32 = 0x0000ffff;
+pub static UPPER_WORD: u32 = 0xffff0000;
