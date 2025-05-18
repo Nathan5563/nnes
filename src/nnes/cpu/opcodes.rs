37b7f28615dbe3b634ea14c53c11099246b1f661
Nathan5563 <ndawit611@gmail.com>
2025-05-18 14:13:23 -0400

    feat: Implemented brk

--- src/nnes/cpu/opcodes.rs
+++ src/nnes/cpu/opcodes.rs
@@ -1,80 +1,87 @@
-use super::CPU;
+use super::{Flags, CPU};
+use crate::utils::{hi_byte, lo_byte};
 
 impl CPU {
     // Official addressing functions
-    pub fn addr_imp(&mut self, subcycle: u8) -> bool {    // Implied
+    pub fn addr_acc(&mut self, subcycle: u8) -> bool {
+        // Accumulator
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_acc(&mut self, subcycle: u8) -> bool {    // Accumulator
+    pub fn addr_imm(&mut self, subcycle: u8) -> bool {
+        // Immediate
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_imm(&mut self, subcycle: u8) -> bool {    // Immediate
+    pub fn addr_zpg(&mut self, subcycle: u8) -> bool {
+        // Zero page
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_zpg(&mut self, subcycle: u8) -> bool {    // Zero page
+    pub fn addr_zpx(&mut self, subcycle: u8) -> bool {
+        // Zero page + X
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_zpx(&mut self, subcycle: u8) -> bool {    // Zero page + X
+    pub fn addr_zpy(&mut self, subcycle: u8) -> bool {
+        // Zero page + Y
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_zpy(&mut self, subcycle: u8) -> bool {    // Zero page + Y
+    pub fn addr_abs(&mut self, subcycle: u8) -> bool {
+        // Absolute
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_abs(&mut self, subcycle: u8) -> bool {    // Absolute
+    pub fn addr_abx(&mut self, subcycle: u8) -> bool {
+        // Absolute + X
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_abx(&mut self, subcycle: u8) -> bool {    // Absolute + X
+    pub fn addr_aby(&mut self, subcycle: u8) -> bool {
+        // Absolute + Y
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_aby(&mut self, subcycle: u8) -> bool {    // Absolute + Y
+    pub fn addr_ind(&mut self, subcycle: u8) -> bool {
+        // Indirect
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_ind(&mut self, subcycle: u8) -> bool {    // Indirect
+    pub fn addr_inx(&mut self, subcycle: u8) -> bool {
+        // Indirect + X
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_inx(&mut self, subcycle: u8) -> bool {    // Indirect + X
+    pub fn addr_iny(&mut self, subcycle: u8) -> bool {
+        // Indirect + Y
         match subcycle {
             _ => {}
         }
         true
     }
-    pub fn addr_iny(&mut self, subcycle: u8) -> bool {    // Indirect + Y
-        match subcycle {
-            _ => {}
-        }
-        true
-    }
-    pub fn addr_rel(&mut self, subcycle: u8) -> bool {    // Relative
+    pub fn addr_rel(&mut self, subcycle: u8) -> bool {
+        // Relative
         match subcycle {
             _ => {}
         }
@@ -84,13 +91,49 @@ impl CPU {
     // Illegal addressing functions
 
     // Official execute functions
+    pub fn brk(&mut self, subcycle: u8) -> bool {
+        match subcycle {
+            0 => {
+                let _ = self.bus.mem_read(self.pc);
+                self.pc = self.pc.wrapping_add(1);
+                false
+            }
+            1 => {
+                self.stack_push(hi_byte(self.pc));
+                false
+            }
+            2 => {
+                self.stack_push(lo_byte(self.pc));
+                false
+            }
+            3 => {
+                if self.software_interrupt {
+                    self.stack_push(self.p.union(Flags::BREAK).bits());
+                } else {
+                    self.stack_push(self.p.difference(Flags::BREAK).bits());
+                }
+                false
+            }
+            4 => {
+                self.cache.lo = self.bus.mem_read(self.cache.vector);
+                false
+            }
+            5 => {
+                self.cache.hi = self.bus.mem_read(self.cache.vector + 1);
+                self.cache.addr = u16::from_le_bytes([self.cache.lo, self.cache.hi]);
+                self.pc = self.cache.addr;
+                self.software_interrupt = false;
+                true
+            }
+            _ => unreachable!()
+        }
+    }
 
     // Illegal execute functions
-    
 }
 
 pub struct OpCode {
-    code: u8,
+    pub code: u8,
     name: String,
     pub decode_fn: Option<fn(&mut CPU, subcycle: u8) -> bool>,
     pub execute_fn: fn(&mut CPU, subcycle: u8) -> bool,
@@ -123,7 +166,7 @@ impl OpCode {
 
 lazy_static! {
     pub static ref opcodes_list: [Option<OpCode>; 256] = [
-        Some(OpCode::new(0x00, "BRK".to_string(), CPU::addr_imp, CPU::brk, false, false, 0)),
+        Some(OpCode::new(0x00, "BRK".to_string(), None,          CPU::brk, false, false, 0)),
         Some(OpCode::new(0x01, "ORA".to_string(), CPU::addr_inx, CPU::ora, false, false, 0)),
         None,
         None,
@@ -131,7 +174,7 @@ lazy_static! {
         Some(OpCode::new(0x05, "ORA".to_string(), CPU::addr_zpg, CPU::ora, false, false, 0)),
         Some(OpCode::new(0x06, "ASL".to_string(), CPU::addr_zpg, CPU::asl, false, false, 0)),
         None,
-        Some(OpCode::new(0x08, "PHP".to_string(), CPU::addr_imp, CPU::php, false, false, 0)),
+        Some(OpCode::new(0x08, "PHP".to_string(), None,          CPU::php, false, false, 0)),
         Some(OpCode::new(0x09, "ORA".to_string(), CPU::addr_imm, CPU::ora, false, false, 0)),
         Some(OpCode::new(0x0A, "ASL".to_string(), CPU::addr_acc, CPU::asl, false, false, 0)),
         None,
@@ -147,7 +190,7 @@ lazy_static! {
         Some(OpCode::new(0x15, "ORA".to_string(), CPU::addr_zpx, CPU::ora, false, false, 0)),
         Some(OpCode::new(0x16, "ASL".to_string(), CPU::addr_zpx, CPU::asl, false, false, 0)),
         None,
-        Some(OpCode::new(0x18, "CLC".to_string(), CPU::addr_imp, CPU::clc, false, false, 0)),
+        Some(OpCode::new(0x18, "CLC".to_string(), None,          CPU::clc, false, false, 0)),
         Some(OpCode::new(0x19, "ORA".to_string(), CPU::addr_aby, CPU::ora, true,  false, 0)),
         None,
         None,
@@ -163,7 +206,7 @@ lazy_static! {
         Some(OpCode::new(0x25, "AND".to_string(), CPU::addr_zpg, CPU::and, false, false, 0)),
         Some(OpCode::new(0x26, "ROL".to_string(), CPU::addr_zpg, CPU::rol, false, false, 0)),
         None,
-        Some(OpCode::new(0x28, "PLP".to_string(), CPU::addr_imp, CPU::plp, false, false, 0)),
+        Some(OpCode::new(0x28, "PLP".to_string(), None,          CPU::plp, false, false, 0)),
         Some(OpCode::new(0x29, "AND".to_string(), CPU::addr_imm, CPU::and, false, false, 0)),
         Some(OpCode::new(0x2A, "ROL".to_string(), CPU::addr_acc, CPU::rol, false, false, 0)),
         None,
@@ -179,7 +222,7 @@ lazy_static! {
         Some(OpCode::new(0x35, "AND".to_string(), CPU::addr_zpx, CPU::and, false, false, 0)),
         Some(OpCode::new(0x36, "ROL".to_string(), CPU::addr_zpx, CPU::rol, false, false, 0)),
         None,
-        Some(OpCode::new(0x38, "SEC".to_string(), CPU::addr_imp, CPU::sec, false, false, 0)),
+        Some(OpCode::new(0x38, "SEC".to_string(), None,          CPU::sec, false, false, 0)),
         Some(OpCode::new(0x39, "AND".to_string(), CPU::addr_aby, CPU::and, true,  false, 0)),
         None,
         None,
@@ -187,7 +230,7 @@ lazy_static! {
         Some(OpCode::new(0x3D, "AND".to_string(), CPU::addr_abx, CPU::and, true,  false, 0)),
         Some(OpCode::new(0x3E, "ROL".to_string(), CPU::addr_abx, CPU::rol, false, false, 0)),
         None,
-        Some(OpCode::new(0x40, "RTI".to_string(), CPU::addr_imp, CPU::rti, false, false, 0)),
+        Some(OpCode::new(0x40, "RTI".to_string(), None,          CPU::rti, false, false, 0)),
         Some(OpCode::new(0x41, "EOR".to_string(), CPU::addr_inx, CPU::eor, false, false, 0)),
         None,
         None,
@@ -195,7 +238,7 @@ lazy_static! {
         Some(OpCode::new(0x45, "EOR".to_string(), CPU::addr_zpg, CPU::eor, false, false, 0)),
         Some(OpCode::new(0x46, "LSR".to_string(), CPU::addr_zpg, CPU::lsr, false, false, 0)),
         None,
-        Some(OpCode::new(0x48, "PHA".to_string(), CPU::addr_imp, CPU::pha, false, false, 0)),
+        Some(OpCode::new(0x48, "PHA".to_string(), None,          CPU::pha, false, false, 0)),
         Some(OpCode::new(0x49, "EOR".to_string(), CPU::addr_imm, CPU::eor, false, false, 0)),
         Some(OpCode::new(0x4A, "LSR".to_string(), CPU::addr_acc, CPU::lsr, false, false, 0)),
         None,
@@ -211,7 +254,7 @@ lazy_static! {
         Some(OpCode::new(0x55, "EOR".to_string(), CPU::addr_zpx, CPU::eor, false, false, 0)),
         Some(OpCode::new(0x56, "LSR".to_string(), CPU::addr_zpx, CPU::lsr, false, false, 0)),
         None,
-        Some(OpCode::new(0x58, "CLI".to_string(), CPU::addr_imp, CPU::cli, false, false, 0)),
+        Some(OpCode::new(0x58, "CLI".to_string(), None,          CPU::cli, false, false, 0)),
         Some(OpCode::new(0x59, "EOR".to_string(), CPU::addr_aby, CPU::eor, true,  false, 0)),
         None,
         None,
@@ -219,7 +262,7 @@ lazy_static! {
         Some(OpCode::new(0x5D, "EOR".to_string(), CPU::addr_abx, CPU::eor, true,  false, 0)),
         Some(OpCode::new(0x5E, "LSR".to_string(), CPU::addr_abx, CPU::lsr, false, false, 0)),
         None,
-        Some(OpCode::new(0x60, "RTS".to_string(), CPU::addr_imp, CPU::rts, false, false, 0)),
+        Some(OpCode::new(0x60, "RTS".to_string(), None,          CPU::rts, false, false, 0)),
         Some(OpCode::new(0x61, "ADC".to_string(), CPU::addr_inx, CPU::adc, false, false, 0)),
         None,
         None,
@@ -227,7 +270,7 @@ lazy_static! {
         Some(OpCode::new(0x65, "ADC".to_string(), CPU::addr_zpg, CPU::adc, false, false, 0)),
         Some(OpCode::new(0x66, "ROR".to_string(), CPU::addr_zpg, CPU::ror, false, false, 0)),
         None,
-        Some(OpCode::new(0x68, "PLA".to_string(), CPU::addr_imp, CPU::pla, false, false, 0)),
+        Some(OpCode::new(0x68, "PLA".to_string(), None,          CPU::pla, false, false, 0)),
         Some(OpCode::new(0x69, "ADC".to_string(), CPU::addr_imm, CPU::adc, false, false, 0)),
         Some(OpCode::new(0x6A, "ROR".to_string(), CPU::addr_acc, CPU::ror, false, false, 0)),
         None,
@@ -243,7 +286,7 @@ lazy_static! {
         Some(OpCode::new(0x75, "ADC".to_string(), CPU::addr_zpx, CPU::adc, false, false, 0)),
         Some(OpCode::new(0x76, "ROR".to_string(), CPU::addr_zpx, CPU::ror, false, false, 0)),
         None,
-        Some(OpCode::new(0x78, "SEI".to_string(), CPU::addr_imp, CPU::sei, false, false, 0)),
+        Some(OpCode::new(0x78, "SEI".to_string(), None,          CPU::sei, false, false, 0)),
         Some(OpCode::new(0x79, "ADC".to_string(), CPU::addr_aby, CPU::adc, true,  false, 0)),
         None,
         None,
@@ -259,9 +302,9 @@ lazy_static! {
         Some(OpCode::new(0x85, "STA".to_string(), CPU::addr_zpg, CPU::sta, false, false, 0)),
         Some(OpCode::new(0x86, "STX".to_string(), CPU::addr_zpg, CPU::stx, false, false, 0)),
         None,
-        Some(OpCode::new(0x88, "DEY".to_string(), CPU::addr_imp, CPU::dey, false, false, 0)),
+        Some(OpCode::new(0x88, "DEY".to_string(), None,          CPU::dey, false, false, 0)),
         None,
-        Some(OpCode::new(0x8A, "TXA".to_string(), CPU::addr_imp, CPU::txa, false, false, 0)),
+        Some(OpCode::new(0x8A, "TXA".to_string(), None,          CPU::txa, false, false, 0)),
         None,
         Some(OpCode::new(0x8C, "STY".to_string(), CPU::addr_abs, CPU::sty, false, false, 0)),
         Some(OpCode::new(0x8D, "STA".to_string(), CPU::addr_abs, CPU::sta, false, false, 0)),
@@ -275,9 +318,9 @@ lazy_static! {
         Some(OpCode::new(0x95, "STA".to_string(), CPU::addr_zpx, CPU::sta, false, false, 0)),
         Some(OpCode::new(0x96, "STX".to_string(), CPU::addr_zpy, CPU::stx, false, false, 0)),
         None,
-        Some(OpCode::new(0x98, "TYA".to_string(), CPU::addr_imp, CPU::tya, false, false, 0)),
+        Some(OpCode::new(0x98, "TYA".to_string(), None,          CPU::tya, false, false, 0)),
         Some(OpCode::new(0x99, "STA".to_string(), CPU::addr_aby, CPU::sta, false, false, 1)),
-        Some(OpCode::new(0x9A, "TXS".to_string(), CPU::addr_imp, CPU::txs, false, false, 0)),
+        Some(OpCode::new(0x9A, "TXS".to_string(), None,          CPU::txs, false, false, 0)),
         None,
         None,
         Some(OpCode::new(0x9D, "STA".to_string(), CPU::addr_abx, CPU::sta, false, false, 1)),
@@ -291,9 +334,9 @@ lazy_static! {
         Some(OpCode::new(0xA5, "LDA".to_string(), CPU::addr_zpg, CPU::lda, false, false, 0)),
         Some(OpCode::new(0xA6, "LDX".to_string(), CPU::addr_zpg, CPU::ldx, false, false, 0)),
         None,
-        Some(OpCode::new(0xA8, "TAY".to_string(), CPU::addr_imp, CPU::tay, false, false, 0)),
+        Some(OpCode::new(0xA8, "TAY".to_string(), None,          CPU::tay, false, false, 0)),
         Some(OpCode::new(0xA9, "LDA".to_string(), CPU::addr_imm, CPU::lda, false, false, 0)),
-        Some(OpCode::new(0xAA, "TAX".to_string(), CPU::addr_imp, CPU::tax, false, false, 0)),
+        Some(OpCode::new(0xAA, "TAX".to_string(), None,          CPU::tax, false, false, 0)),
         None,
         Some(OpCode::new(0xAC, "LDY".to_string(), CPU::addr_abs, CPU::ldy, false, false, 0)),
         Some(OpCode::new(0xAD, "LDA".to_string(), CPU::addr_abs, CPU::lda, false, false, 0)),
@@ -307,9 +350,9 @@ lazy_static! {
         Some(OpCode::new(0xB5, "LDA".to_string(), CPU::addr_zpx, CPU::lda, false, false, 0)),
         Some(OpCode::new(0xB6, "LDX".to_string(), CPU::addr_zpy, CPU::ldx, false, false, 0)),
         None,
-        Some(OpCode::new(0xB8, "CLV".to_string(), CPU::addr_imp, CPU::clv, false, false, 0)),
+        Some(OpCode::new(0xB8, "CLV".to_string(), None,          CPU::clv, false, false, 0)),
         Some(OpCode::new(0xB9, "LDA".to_string(), CPU::addr_aby, CPU::lda, true,  false, 0)),
-        Some(OpCode::new(0xBA, "TSX".to_string(), CPU::addr_imp, CPU::tsx, false, false, 0)),
+        Some(OpCode::new(0xBA, "TSX".to_string(), None,          CPU::tsx, false, false, 0)),
         None,
         Some(OpCode::new(0xBC, "LDY".to_string(), CPU::addr_abx, CPU::ldy, true,  false, 0)),
         Some(OpCode::new(0xBD, "LDA".to_string(), CPU::addr_abx, CPU::lda, true,  false, 0)),
@@ -323,9 +366,9 @@ lazy_static! {
         Some(OpCode::new(0xC5, "CMP".to_string(), CPU::addr_zpg, CPU::cmp, false, false, 0)),
         Some(OpCode::new(0xC6, "DEC".to_string(), CPU::addr_zpg, CPU::dec, false, false, 0)),
         None,
-        Some(OpCode::new(0xC8, "INY".to_string(), CPU::addr_imp, CPU::iny, false, false, 0)),
+        Some(OpCode::new(0xC8, "INY".to_string(), None,          CPU::iny, false, false, 0)),
         Some(OpCode::new(0xC9, "CMP".to_string(), CPU::addr_imm, CPU::cmp, false, false, 0)),
-        Some(OpCode::new(0xCA, "DEX".to_string(), CPU::addr_imp, CPU::dex, false, false, 0)),
+        Some(OpCode::new(0xCA, "DEX".to_string(), None,          CPU::dex, false, false, 0)),
         None,
         Some(OpCode::new(0xCC, "CPY".to_string(), CPU::addr_abs, CPU::cpy, false, false, 0)),
         Some(OpCode::new(0xCD, "CMP".to_string(), CPU::addr_abs, CPU::cmp, false, false, 0)),
@@ -339,7 +382,7 @@ lazy_static! {
         Some(OpCode::new(0xD5, "CMP".to_string(), CPU::addr_zpx, CPU::cmp, false, false, 0)),
         Some(OpCode::new(0xD6, "DEC".to_string(), CPU::addr_zpx, CPU::dec, false, false, 0)),
         None,
-        Some(OpCode::new(0xD8, "CLD".to_string(), CPU::addr_imp, CPU::cld, false, false, 0)),
+        Some(OpCode::new(0xD8, "CLD".to_string(), None,          CPU::cld, false, false, 0)),
         Some(OpCode::new(0xD9, "CMP".to_string(), CPU::addr_aby, CPU::cmp, true,  false, 0)),
         None,
         None,
@@ -355,9 +398,9 @@ lazy_static! {
         Some(OpCode::new(0xE5, "SBC".to_string(), CPU::addr_zpg, CPU::sbc, false, false, 0)),
         Some(OpCode::new(0xE6, "INC".to_string(), CPU::addr_zpg, CPU::inc, false, false, 0)),
         None,
-        Some(OpCode::new(0xE8, "INX".to_string(), CPU::addr_imp, CPU::inx, false, false, 0)),
+        Some(OpCode::new(0xE8, "INX".to_string(), None,          CPU::inx, false, false, 0)),
         Some(OpCode::new(0xE9, "SBC".to_string(), CPU::addr_imm, CPU::sbc, false, false, 0)),
-        Some(OpCode::new(0xEA, "NOP".to_string(), CPU::addr_imp, CPU::nop, false, false, 0)),
+        Some(OpCode::new(0xEA, "NOP".to_string(), None,          CPU::nop, false, false, 0)),
         None,
         Some(OpCode::new(0xEC, "CPX".to_string(), CPU::addr_abs, CPU::cpx, false, false, 0)),
         Some(OpCode::new(0xED, "SBC".to_string(), CPU::addr_abs, CPU::sbc, false, false, 0)),
@@ -371,7 +414,7 @@ lazy_static! {
         Some(OpCode::new(0xF5, "SBC".to_string(), CPU::addr_zpx, CPU::sbc, false, false, 0)),
         Some(OpCode::new(0xF6, "INC".to_string(), CPU::addr_zpx, CPU::inc, false, false, 0)),
         None,
-        Some(OpCode::new(0xF8, "SED".to_string(), CPU::addr_imp, CPU::sed, false, false, 0)),
+        Some(OpCode::new(0xF8, "SED".to_string(), None,          CPU::sed, false, false, 0)),
         Some(OpCode::new(0xF9, "SBC".to_string(), CPU::addr_aby, CPU::sbc, true,  false, 0)),
         None,
         None,
